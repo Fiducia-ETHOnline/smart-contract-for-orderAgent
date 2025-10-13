8c68584b08623a56f83bfddf3650255513f2d7d0
Axel Valavaara <acke@Axels-MacBook-Air.local>
2025-10-13 13:10:47 +0300

    more tests

--- src/OrderContract.sol
+++ src/OrderContract.sol
@@ -35,6 +35,7 @@ contract OrderContract is ReentrancyGuard{
     error OrderContract__OrderCannotBeCanceledInCurrentState();
     error OrderContract__OrderCannotBeFinalizedInCurrentState();
     error OrderContract__EnoughTimeHasNotPassed();
+    error OrderContract__CannotProposeOrderAnswerInCurrentState();
 
     /* type declarations */
     enum OrderStatus {
@@ -44,18 +45,35 @@ contract OrderContract is ReentrancyGuard{
         Completed,
         Cancelled      
     }
+
+    struct Offer {
+        address buyer;
+        bytes32 promptHash;
+        bytes32 answerHash;
+        uint256 price; 
+        uint256 paid; 
+        uint256 timestamp;
+        OrderStatus status;
+    }
     /* state variables */
     uint256 private constant AGENT_FEE = 1 ether; // Fee for agent services. Adjust as needed.
     uint256 private constant HOLD_UNTIL = 600; // Time in seconds to hold the order. Adjust as needed.
     uint64 private offerID = 0; // Counter for offer IDs
     address private immutable agentController; // Address of the agent controller
     address private immutable pyUSD; // Address of the pyUSD token contract
-    mapping(address => mapping(uint64 offerId => bytes32 promptHash)) private addressToOfferIdToPromptHash;
-    mapping(uint64 offerId => uint256 timestamp) private offerIdToTimestamp;
-    mapping(uint64 offerId => mapping(address user => uint256 amountPaid)) private offerIdToUserToAmountPaid;
-    mapping(uint64 offerId => OrderStatus status) private offerIdToStatus;
-    mapping(uint64 offerId => bytes32 answerHash) private offerIdToAnswerHash;
-    mapping(uint64 => address) private offerIdToUser;
+    
+    // will make mappings to struct!!! thus too messy.
+    //////////////////////////////////////////////
+    //  FIX THIS!!!!
+    ///////////////////////////////////
+    // mapping(address => mapping(uint64 offerId => bytes32 promptHash)) private addressToOfferIdToPromptHash;
+    // mapping(uint64 offerId => uint256 timestamp) private offerIdToTimestamp;
+    // mapping(uint64 offerId => mapping(address user => uint256 amountPaid)) private offerIdToUserToAmountPaid;
+    // mapping(uint64 offerId => OrderStatus status) private offerIdToStatus;
+    // mapping(uint64 offerId => bytes32 answerHash) private offerIdToAnswerHash;
+    // mapping(uint64 => address) private offerIdToUser;
+
+    mapping(uint64 => Offer) public offers;
     /* events */
     event OrderProposed(address indexed user, uint64 indexed offerId, bytes32 indexed promptHash);
     event OrderConfirmed(address indexed user, uint64 indexed offerId, uint256 indexed amountPaid);
@@ -70,7 +88,7 @@ contract OrderContract is ReentrancyGuard{
     }
     modifier onlyUserWithOffer(uint64 offerId) {
         // Ensure the caller has a valid offer
-        if (addressToOfferIdToPromptHash[msg.sender][offerId] == bytes32(0)) {
+        if (offers[offerId].buyer != msg.sender) {
             revert OrderContract__userHasNoAccessToOffer();
         }
         _;
@@ -82,13 +100,17 @@ contract OrderContract is ReentrancyGuard{
         pyUSD = pyUSDAddress;
     }
     function proposeOrder(bytes32 promptHash) external returns(uint64 offerId) {
+        
         // Increment the offer ID counter
         offerID++;
 
-        offerIdToStatus[offerID] = OrderStatus.InProgress;
-        addressToOfferIdToPromptHash[msg.sender][offerID] = promptHash;
-        offerIdToUser[offerID] = msg.sender;
-        emit OrderProposed(msg.sender, offerID, promptHash);
+        // offerIdToStatus[offerID] = OrderStatus.InProgress;
+        offers[offerID].status = OrderStatus.InProgress;
+        offers[offerID].buyer = msg.sender;
+        offers[offerID].promptHash = promptHash;
+        // addressToOfferIdToPromptHash[msg.sender][offerID] = promptHash;
+        // offerIdToUser[offerID] = msg.sender;
+        emit OrderProposed(offers[offerID].buyer, offerID, offers[offerID].promptHash);
         
         return offerID;
         
@@ -97,16 +119,19 @@ contract OrderContract is ReentrancyGuard{
 
 
     function confirmOrder(uint64 offerId, uint256 priceForOffer) external nonReentrant onlyUserWithOffer(offerId) {
-        if ( offerIdToStatus[offerId] != OrderStatus.Proposed) {
+        if ( offers[offerId].status != OrderStatus.Proposed) {
             revert OrderContract__OrderCannotBeConfirmedInCurrentState();
         }
         // update amount paid for the offer by the user
         uint256 amountToPay = priceForOffer + AGENT_FEE;
-        offerIdToUserToAmountPaid[offerId][msg.sender] += amountToPay;
-        offerIdToStatus[offerId] = OrderStatus.Confirmed;
+        // offerIdToUserToAmountPaid[offerId][msg.sender] += amountToPay;
+        // offerIdToStatus[offerId] = OrderStatus.Confirmed;
+        offers[offerId].paid = amountToPay;
+        offers[offerId].status = OrderStatus.Confirmed;
         // Record the current timestamp for the offer
-        offerIdToTimestamp[offerId] = block.timestamp;
-        bool success= ERC20(pyUSD).transferFrom(msg.sender, address(this), priceForOffer + AGENT_FEE);
+        // offerIdToTimestamp[offerId] = block.timestamp;
+        offers[offerId].timestamp = block.timestamp;
+        bool success= ERC20(pyUSD).transferFrom(msg.sender, address(this), amountToPay);
         
         if (!success) {
             revert OrderContract__ERC20TransferFailed();
@@ -118,18 +143,24 @@ contract OrderContract is ReentrancyGuard{
 
     // Save answer from propose order. Answer will be the proposed order. Set orderStatus to proposed.
     function proposeOrderAnswer(bytes32 answerHash, uint64 offerId) external onlyAgentController {
-        offerIdToAnswerHash[offerId] = answerHash;
-        offerIdToStatus[offerId] = OrderStatus.Proposed;
+        if (offers[offerId].status != OrderStatus.InProgress) {
+            revert OrderContract__CannotProposeOrderAnswerInCurrentState();
+        }
+        // offerIdToAnswerHash[offerId] = answerHash;
+        // offerIdToStatus[offerId] = OrderStatus.Proposed;
+        offers[offerId].answerHash = answerHash;
+        offers[offerId].status = OrderStatus.Proposed;
     }
 
     function finalizeOrder (uint64 offerId) external onlyAgentController returns(bool){
-        if (offerIdToStatus[offerId] != OrderStatus.Confirmed) {
+        if (offers[offerId].status != OrderStatus.Confirmed) {
             revert OrderContract__OrderCannotBeFinalizedInCurrentState();
         }
-        offerIdToStatus[offerId] = OrderStatus.Completed;
-        uint256 amountPaid = offerIdToUserToAmountPaid[offerId][msg.sender];
+        // offerIdToStatus[offerId] = OrderStatus.Completed;
+        offers[offerId].status = OrderStatus.Completed;
+        uint256 amountPaid = offers[offerId].paid;
         emit orderFinalized(getUserByOfferId(offerId), offerId);
-        bool success = ERC20(pyUSD).transfer(getUserByOfferId(offerId), amountPaid - AGENT_FEE);
+        bool success = ERC20(pyUSD).transfer(agentController, amountPaid);
         if (!success) {
             revert OrderContract__ERC20TransferFailed();
         }
@@ -139,14 +170,15 @@ contract OrderContract is ReentrancyGuard{
     }
 
     function cancelOrder(uint64 offerId) external onlyUserWithOffer(offerID) {
-        if (block.timestamp - offerIdToTimestamp[offerId] < HOLD_UNTIL) {
+        if (block.timestamp - offers[offerId].timestamp < HOLD_UNTIL) {
             revert OrderContract__EnoughTimeHasNotPassed();
         }
-        if (offerIdToStatus[offerId] != OrderStatus.Confirmed) {
+        if (offers[offerId].status != OrderStatus.Confirmed) {
             revert OrderContract__OrderCannotBeCanceledInCurrentState();
         }
-        offerIdToStatus[offerId] = OrderStatus.Cancelled;
-        bool success = ERC20(pyUSD).transfer(msg.sender, offerIdToUserToAmountPaid[offerID][msg.sender]);
+        // offerIdToStatus[offerId] = OrderStatus.Cancelled;
+        offers[offerId].status = OrderStatus.Cancelled;
+        bool success = ERC20(pyUSD).transfer(msg.sender, offers[offerId].paid);
         if (!success) {
             revert OrderContract__ERC20TransferFailed();
         }
@@ -155,12 +187,12 @@ contract OrderContract is ReentrancyGuard{
 
 
     /* getter functions */
-    function getPromptHash(address user, uint64 offerId) external view returns (bytes32) {
-        return addressToOfferIdToPromptHash[user][offerId];
+    function getPromptHash(uint64 offerId) external view returns (bytes32) {
+        return offers[offerId].promptHash;
     }
     
-    function getAmountPaid(uint64 offerId, address user) external view returns (uint256) {
-        return offerIdToUserToAmountPaid[offerId][user];
+    function getAmountPaid(uint64 offerId) external view returns (uint256) {
+        return offers[offerId].paid;
     }
 
     function getAgentController() external view returns (address) {
@@ -172,13 +204,21 @@ contract OrderContract is ReentrancyGuard{
     }
 
     function getOfferIdToTimestamp(uint64 offerId) external view returns (uint256) {
-        return offerIdToTimestamp[offerId];
+        return offers[offerId].timestamp;
     }
 
 
 
     function getUserByOfferId(uint64 offerId) public view returns (address) {
-        return offerIdToUser[offerId];
+        return offers[offerId].buyer;
+    }
+
+    function getOfferStatus(uint64 offerId) external view returns (OrderStatus) {
+        return offers[offerId].status;
+    }
+    
+    function getAnswerHash(uint64 offerId) external view returns (bytes32) {
+        return offers[offerId].answerHash;
     }
 
 
--- test/unit/OrderContractTest.t.sol
+++ test/unit/OrderContractTest.t.sol
@@ -43,7 +43,7 @@ contract OrderContractTest is Test {
         uint64 offerId = orderContract.proposeOrder(promptHash);
         vm.stopPrank();
         // Assert
-        bytes32 storedPromptHash = orderContract.getPromptHash(USER, offerId);
+        bytes32 storedPromptHash = orderContract.getPromptHash(offerId);
         assertEq(storedPromptHash, promptHash);
         assertEq(offerId, expectedOfferId);
     }
@@ -104,7 +104,7 @@ contract OrderContractTest is Test {
         orderContract.confirmOrder(offerId, priceForOffer);
         vm.stopPrank();
         // Assert
-        uint256 amountPaid = orderContract.getAmountPaid(offerId, USER);
+        uint256 amountPaid = orderContract.getAmountPaid(offerId);
         assertEq(amountPaid, expectedAmountPaid);
         assertEq(orderContract.getOfferIdToTimestamp(offerId), expectedTimeStamp);
     }
@@ -124,6 +124,161 @@ contract OrderContractTest is Test {
         orderContract.confirmOrder(offerId, priceForOffer);
         vm.stopPrank();
     }
+     //////////////////////////////////////
+    //        proposeOrderAnswer tests    //
+    //////////////////////////////////////
+
+    function testOnlyControllerCanProposeAnswer() public proposeOrderForUser {
+        // Arrange
+        uint64 offerId = 1;
+        bytes32 answerHash = keccak256(abi.encodePacked("Test Answer"));
+        // Act / Assert
+        vm.prank(address(0x123));
+        vm.expectRevert(OrderContract.OrderContract__notAgentController.selector);
+        orderContract.proposeOrderAnswer(answerHash, offerId);   
+    }
+    
+    function testProposeOrderAnswerUpdatesState() public proposeOrderForUser {
+        // Arrange
+        uint64 offerId = 1;
+        bytes32 answerHash = keccak256(abi.encodePacked("Test Answer"));
+        // Act
+        vm.prank(addressController);
+        orderContract.proposeOrderAnswer(answerHash, offerId);   
+        // Assert
+        bytes32 storedAnswerHash = orderContract.getAnswerHash(offerId);
+        assertEq(storedAnswerHash, answerHash);
+        OrderContract.OrderStatus status = orderContract.getOfferStatus(offerId);
+        assertEq(uint8(status), uint8(OrderContract.OrderStatus.Proposed));
+    }
+
+    
+     //////////////////////////////////////
+    //        finalizeOrder tests   //
+    //////////////////////////////////////
+
+    modifier orderConfirmed() {
+        vm.startPrank(USER);
+        orderContract.proposeOrder(promptHash);
+        vm.stopPrank();
+        vm.prank(addressController);
+        orderContract.proposeOrderAnswer(keccak256(abi.encodePacked("Test Answer")), 1);
+        uint256 expectedAmountPaid = 5 ether + orderContract.getAgentFee();
+        vm.startPrank(USER);
+        ERC20Mock(pyUSD).approve(address(orderContract), expectedAmountPaid);
+        orderContract.confirmOrder(1, 5 ether);
+        vm.stopPrank();
+        _;
+    }
+     
+    function testFinalizeOrderCanOnlyBeCalledWhenStateIsConfirmed() public {
+        // Arrange
+        uint64 offerId = 1;
+        // Act / Assert
+        vm.prank(addressController);
+        vm.expectRevert(OrderContract.OrderContract__OrderCannotBeFinalizedInCurrentState.selector);
+        orderContract.finalizeOrder(offerId);   
+    }
+
+    function testFinalizeOrderUpdatesState() public orderConfirmed {
+        // Arrange
+        uint64 offerId = 1;
+        // Act
+        vm.prank(addressController);
+        orderContract.finalizeOrder(offerId);   
+        // Assert
+        OrderContract.OrderStatus status = orderContract.getOfferStatus(offerId);
+        assertEq(uint8(status), uint8(OrderContract.OrderStatus.Completed));
+    }
+
+    function testFinalizeOrderPaysController() public orderConfirmed {
+        // Arrange
+        uint64 offerId = 1;
+        uint256 controllerBalanceBefore = ERC20Mock(pyUSD).balanceOf(addressController);
+        uint256 expectedAmountPaid = 5 ether + orderContract.getAgentFee();
+        // Act
+        vm.prank(addressController);
+        orderContract.finalizeOrder(offerId);   
+        uint256 controllerBalanceAfter = ERC20Mock(pyUSD).balanceOf(addressController);
+        // Assert
+        assertEq(controllerBalanceAfter - controllerBalanceBefore, expectedAmountPaid);
+    }
+
+
+    //////////////////////////////////////////
+    ///     Big tests with loops            ///
+    //////////////////////////////////////////
+
+    modifier multipleUserProposeOrder(uint256 numberOfUsers) {
+        for (uint256 i = 0; i < numberOfUsers; i++) {
+            address user = makeAddr(string(abi.encodePacked("user", vm.toString(i))));
+            vm.startPrank(user);
+            ERC20Mock(pyUSD).mint(user, 100 ether);
+            ERC20Mock(pyUSD).approve(address(orderContract), amountForCompute);
+            orderContract.proposeOrder(promptHash);
+            vm.stopPrank();
+        }
+        _;
+    }
+
+    function testMultipleUserProposeOrder() public {
+        // Arrange
+        uint256 numberOfUsers = 10;
+        uint256 expectedOfferId = 1;
+        // Act
+        for (uint256 i = 0; i < numberOfUsers; i++) {
+            address user = makeAddr(string(abi.encodePacked("user", vm.toString(i))));
+            vm.startPrank(user);
+            uint64 offerId = orderContract.proposeOrder(promptHash);
+            vm.stopPrank();
+            // Assert
+            bytes32 storedPromptHash = orderContract.getPromptHash(offerId);
+            assertEq(storedPromptHash, promptHash);
+            assertEq(offerId, expectedOfferId);
+            expectedOfferId++;
+        }
+    }
+
+    function testMultipleProposeOrderAnswer() public multipleUserProposeOrder(10) {
+        // Arrange
+        uint256 numberOfUsers = 10;
+        bytes32 answerHash = keccak256(abi.encodePacked("Test Answer"));
+        // Act
+        for (uint256 i = 0; i < numberOfUsers; i++) {
+            uint64 offerId = uint64(i + 1);
+            vm.prank(addressController);
+            orderContract.proposeOrderAnswer(answerHash, offerId);   
+            // Assert
+            bytes32 storedAnswerHash = orderContract.getAnswerHash(offerId);
+            assertEq(storedAnswerHash, answerHash);
+            OrderContract.OrderStatus status = orderContract.getOfferStatus(offerId);
+            assertEq(uint8(status), uint8(OrderContract.OrderStatus.Proposed));
+        }
+    }
+    function testMultipleConfirmOrder() public multipleUserProposeOrder(10) {
+        // Arrange
+        uint256 numberOfUsers = 10;
+        bytes32 answerHash = keccak256(abi.encodePacked("Test Answer"));
+        uint256 priceForOffer = 5 ether;
+        uint256 expectedAmountPaid = priceForOffer + orderContract.getAgentFee(); // AGENT_FEE i 1 ether
+        // Act
+        for (uint256 i = 0; i < numberOfUsers; i++) {
+            uint64 offerId = uint64(i + 1);
+            address user = makeAddr(string(abi.encodePacked("user", vm.toString(i))));
+            vm.prank(addressController);
+            orderContract.proposeOrderAnswer(answerHash, offerId);   
+            vm.startPrank(user);
+            ERC20Mock(pyUSD).approve(address(orderContract), expectedAmountPaid);
+            orderContract.confirmOrder(offerId, priceForOffer);
+            vm.stopPrank();
+            // Assert
+            uint256 amountPaid = orderContract.getAmountPaid(offerId);
+            assertEq(amountPaid, expectedAmountPaid);
+            assertEq(orderContract.getOfferIdToTimestamp(offerId), block.timestamp);
+        }
+    }
+
+    function testMulitpleCanFinalizeOrder
 
 
 }
